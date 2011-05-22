ae5390fb64ff094fb1c808117ecf0749b740923b
Nijiko Yonskai <me@nijikokun.com>
2011-05-22 16:34:11 -0400

    Initial Release 1/5

---
+++ README.md
@@ -0,0 +1,44 @@
+     
+    # |_|  _   _ |_   _   _ 1\5
+      | | (_| _) | | |/_ | 
+
+Hasher is a small but powerful URL Hash Routing script. It allows you to easily manage and create routes as well as apply object and functions to those routes.
+
+Usage:
+
+    // Setup the Hasher
+    Hasher.setup();
+    
+    // Add a route
+    Hasher.add("name/:name", function(name) {
+        // Alert
+        alert(name);
+        
+        // Console
+        console.log(name);
+    });
+    
+<h3>Licensing?</h3>
+
+Hasher is copyrighted / licensed under the MIT License.
+
+    Copyright (c) 2011 Nijikokun <nijikokun@gmail.com>, http://nexua.org
+
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files (the
+    "Software"), to deal in the Software without restriction, including
+    without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to
+    the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ hasher.js
@@ -0,0 +1,137 @@
+/*
+ * # |_|  _   _ |_   _   _ 1\5
+ *   | | (_| _) | | |/_ | 
+ * 
+ * Url Hash Routing made easy.
+ *
+ * @author: Nijikokun <nijikokun@gmail.com>
+ * @copyright: May 22, 2011
+ * @version: 0.2
+ * @license: MIT License <>
+ */
+
+var Hasher = {
+    routes: [],
+    
+    regex : {
+        HASH_STRIP: /^#*/,
+        ARG_NAMED: /:([\w\d]+)/g,
+        ARG_SPLAT: /\*([\w\d]+)/g,
+        ESC: /[-[\]{}()+?.,\\^$|#\s]/g
+    },
+    
+    history: {
+        cache: false,
+        support: ("history" in window)
+    },
+    
+    marray: function(args){
+        return Array.prototype.slice.call(args, 0);
+    },
+    
+    proxy: function(o){
+        var me = this;
+        return(function(){ 
+            return o.apply(me, arguments); 
+        });
+    },
+    
+    proxyAll: function(){
+        var functions = this.marray(arguments);
+      
+        for (var i=0; i < functions.length; i++)
+            this[functions[i]] = this.proxy(this[functions[i]]);
+    },
+        
+    add: function(path, callback){
+        if (typeof path == "object") {
+            for(var p in path) this.add(p, path[p]);
+        } else {
+            if (typeof path == "string") {      
+                path = path.replace(this.regex.ESC, "\\$&").replace(this.regex.ARG_NAMED, "([^\/]*)").replace(this.regex.ARG_SPLAT, "(.*?)");
+                path = new RegExp('^' + path + '$');
+            }
+        
+            this.routes.push({
+                'route': path, 
+                'callback': callback
+            });
+        }
+    },
+    
+    setup: function(options){
+        if (options && options.history)
+            this.history.cache = this.history.support && options.history;
+        
+        if ( this.history.cache )
+            $(window).bind("popstate", this.change);
+        else
+            $(window).bind("hashchange", this.change);
+        
+        this.proxyAll("change");
+        this.change();
+    },
+    
+    unbind: function(){
+        if (this.history)
+            $(window).unbind("popstate", this.change);
+        else
+            $(window).unbind("hashchange", this.change);
+    },
+    
+    navigate: function(){
+        var args = makeArray(arguments);
+        var triggers = false;
+      
+        if (typeof args[args.length - 1] == "boolean") {
+            triggers = args.pop();
+        }
+      
+        var path = args.join("/");      
+        if (this.path == path) return;
+      
+        if ( !triggers )
+            this.path = path;
+      
+        if (this.history.cache)
+            history.cache.pushState({}, document.title, this.getHost() + path);
+        else
+            window.location.hash = path;
+    },
+    
+    match: function(path, route, callback){
+        var match = route.exec(path);
+        if (!match) return false;
+        var params = match.slice(1);
+        callback.apply(callback, params);
+        return true;
+    },
+    
+    getPath: function(){
+        return window.location.pathname;
+    },
+    
+    getHash: function(){
+        return window.location.hash;
+    },
+    
+    getHost: function(){
+        return((document.location + "").replace(
+            this.getPath() + this.getHash(), ""
+        ));
+    },
+    
+    getFragment: function(){
+        return this.getHash().replace(this.regex.HASH_STRIP, "");
+    },
+    
+    change: function(e){
+        var path = (this.history.cache ? this.getPath() : this.getFragment());
+        if (path == this.path) return;
+        this.path = path;
+        for (var i=0; i < this.routes.length; i++) {
+            var r = this.routes[i];
+            if (this.match(path, r['route'], r['callback'])) return;
+        }
+    }
+};
